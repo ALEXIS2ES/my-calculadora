216774070caf5f1c8afc582ef3477227f0b81ec3
ALEXIS2ES <vccamerccoa@unac.edu.co>
2020-07-14 11:12:43 -0500

    añadir evento click en el drawable

--- app/src/main/java/com/lhamfor/mycalculadora/MainActivity.java
+++ app/src/main/java/com/lhamfor/mycalculadora/MainActivity.java
@@ -2,6 +2,7 @@ package com.lhamfor.mycalculadora;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -40,6 +41,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 InputMethodManager input = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 input.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        });
+        etInput.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == motionEvent.ACTION_UP){
+                    if (motionEvent.getRawX() >= (etInput.getRight() -
+                    etInput.getCompoundDrawables() [Constantes.DRAWABLE_RIGHT].getBounds().width())){
+                        if (etInput.length() > 0){
+                            final  int length = etInput.getText().length();
+                            etInput.getText().delete(length-1, length);
+                        }
+                    }
+                    return true;
+                }
+                return false;
             }
         });
     }
