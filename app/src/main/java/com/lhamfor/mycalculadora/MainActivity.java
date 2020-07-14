6465363eb892454b6f7c1874131b1fe3ed86ad4b
ALEXIS2ES <vccamerccoa@unac.edu.co>
2020-07-13 19:57:40 -0500

    evento click

--- app/src/main/java/com/lhamfor/mycalculadora/MainActivity.java
+++ app/src/main/java/com/lhamfor/mycalculadora/MainActivity.java
@@ -1,6 +1,8 @@
 package com.lhamfor.mycalculadora;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
@@ -8,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +25,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+    }
+
+    @OnClick({R.id.btnSeven, R.id.btnFour, R.id.btnOne, R.id.btnPoint, R.id.btnEight, R.id.btnFive,
+            R.id.btntwo, R.id.btnZero, R.id.btnNine, R.id.btnSix, R.id.btnThree})
+    public void inClickNumbers(View view) {
+        final String valStr = ((Button)view).getText().toString();
+        switch (view.getId()) {
+            case R.id.btnZero:
+            case R.id.btnOne:
+            case R.id.btntwo:
+            case R.id.btnThree:
+            case R.id.btnFour:
+            case R.id.btnFive:
+            case R.id.btnSix:
+            case R.id.btnSeven:
+            case R.id.btnEight:
+            case R.id.btnNine:
+                etInput.getText().append(valStr);
+                break;
+            case R.id.btnPoint:
+                break;
+        }
     }
 }
