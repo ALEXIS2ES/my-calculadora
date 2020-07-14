9d2281c4c20ec4c1de7ec39db28d59ff4d08ea49
ALEXIS2ES <vccamerccoa@unac.edu.co>
2020-07-13 19:31:31 -0500

    Clases auxiliares

--- app/src/main/java/com/lhamfor/mycalculadora/MainActivity.java
+++ app/src/main/java/com/lhamfor/mycalculadora/MainActivity.java
@@ -1,14 +1,26 @@
 package com.lhamfor.mycalculadora;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etInput)
+    EditText etInput;
+    @BindView(R.id.contenMain)
+    RelativeLayout contenMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
---
+++ app/src/main/java/com/lhamfor/mycalculadora/OnResolveCallback.java
@@ -0,0 +1,6 @@
+package com.lhamfor.mycalculadora;
+
+public interface OnResolveCallback {
+    void onShowMessage(int errorRes);
+    void onIsEditing();
+}
